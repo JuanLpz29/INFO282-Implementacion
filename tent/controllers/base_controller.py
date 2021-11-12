f5e29cf50b67a9c2aaa154bdb49b2521895b0be0
hiawvp <josevasquez95p@gmail.com>
2021-11-12 03:09:44 -0300

    actualizar entrada

--- tent/controllers/base_controller.py
+++ tent/controllers/base_controller.py
@@ -1,6 +1,6 @@
 def hello():
     return """
      <iframe autostart="1" width="1200" height="780"
-src="https://www.youtube.com/embed/tgbNymZ7vqY">
+src="https://www.youtube.com/watch?v=YtrnoLxiExI">
 </iframe> 
     """
