31dec979e0405abd130058ec2dfa7539029d4ec0
hiawvp <josevasquez95p@gmail.com>
2021-12-03 02:22:48 -0300

    archivos para generar ejecutable

--- front/vite.config.js
+++ front/vite.config.js
@@ -5,8 +5,8 @@ const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: path.resolve(__dirname, "./dist/"),
-    // base: "./",
+    // base: path.resolve(__dirname, "./dist/"),
+    base: "./",
     plugins: [
         vue({
             template: { transformAssetUrls }
