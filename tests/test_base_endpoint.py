f128f9af9d1389a9ede87c63e4fc1be720ee2f9e
hiawvp <josevasquez95p@gmail.com>
2022-04-05 22:04:52 -0400

    update tests

--- tests/test_base_endpoint.py
+++
@@ -1,4 +0,0 @@
-def test_hello(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b"API EN CONSTRUCCION" in response.data
--- tests/test_compras_endpoint.py
+++
@@ -1,8 +0,0 @@
-def test_get_compras(client):
-    response = client.get('/compras/')
-    assert response.status_code == 200
-
-
-def test_get_compra_id(client):
-    response = client.get('/compras/2')
-    assert response.status_code == 200
