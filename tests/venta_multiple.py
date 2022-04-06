3cc36dc8665f612ddda10ecf3ee470955ddd16e8
hiawvp <josevasquez95p@gmail.com>
2022-04-05 23:02:47 -0400

    add test02

---
+++ tests/venta_multiple.py
@@ -0,0 +1,21 @@
+import requests
+from concurrent.futures import ThreadPoolExecutor
+
+workers = 4
+
+
+def post_url(args):
+    return requests.post(args)
+
+
+url = 'https://hiawvp.pythonanywhere.com/ventas/?nombre=tester'
+
+list_of_urls = [f"{url}{i}&codigoBarra=7804300122140" for i in range(workers)]
+
+
+with ThreadPoolExecutor(max_workers=workers) as pool:
+    response_list = list(pool.map(post_url, list_of_urls))
+
+
+for response in response_list:
+    print(response, response.json())
