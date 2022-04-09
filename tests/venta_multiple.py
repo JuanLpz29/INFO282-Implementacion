1924024b1a355d154868b433553a8deed6f79710
hiawvp <josevasquez95p@gmail.com>
2022-04-08 23:27:17 -0400

    update test details

--- tests/venta_multiple.py
+++ tests/venta_multiple.py
@@ -7,7 +7,7 @@ workers = 4
 def post_url(args):
     return requests.post(args)
 
-
+# url = 'http://127.0.0.1:5000/ventas/?nombre=tester'
 url = 'https://hiawvp.pythonanywhere.com/ventas/?nombre=tester'
 
 list_of_urls = [f"{url}{i}&codigoBarra=7804300122140" for i in range(workers)]
